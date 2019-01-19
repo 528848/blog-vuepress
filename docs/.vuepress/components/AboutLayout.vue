9b59fb7425bfc04a3957dfe6b6520ae021f1a19b
codeteenage <1830702951@qq.com>
2019-01-19 18:51:47 +0800

    Initial commit

---
+++ docs/.vuepress/components/AboutLayout.vue
@@ -0,0 +1,55 @@
+<template>
+    <el-container direction="vertical">
+      <div class="main">
+            <el-card class="box-card">
+              <h1 align="center">关于我</h1>
+              <p>
+                
+              </p>
+
+            </el-card>
+      </div>
+    </el-container>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    
+  }
+};
+</script>
+<style scoped>
+@media (min-width: 1201px) {
+  .main {
+    padding-top: 50px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .box-card{
+    margin: 20px;
+    width: 100%;
+    height: 100%;
+  }
+}
+
+@media (max-width: 1020px) {
+  .main {
+    padding-top: 20px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .box-card{
+    margin: 20px;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
+
---
+++ docs/.vuepress/components/IDELayout.vue
@@ -0,0 +1,63 @@
+<template>
+    <el-container direction="vertical">
+      <div class="main">
+            <div class="marvel-device macbook">
+    <div class="top-bar"></div>
+    <div class="camera"></div>
+    <div class="screen">
+        
+    </div>
+    <div class="bottom-bar"></div>
+</div>
+      </div>
+    </el-container>
+</template>
+<script>
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+  mounted(){
+    
+    
+  },
+  methods: {
+
+  }
+};
+</script>
+<style scoped>
+@media (min-width: 1201px) {
+  .main {
+    padding-top: 50px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .box-card {
+    margin: 20px;
+    width: 100%;
+    height: 100%;
+  }
+}
+
+@media (max-width: 1020px) {
+  .main {
+    padding-top: 20px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .box-card {
+    margin: 20px;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
+
